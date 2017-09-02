bbb9e0c7b60c6b281c521a9e468fad529e524d53
S.Strizhov <beowolfrus@gmail.com>
2017-09-02 13:40:50 +0400

    Base skeleton of parser

--- IBoxUpdater/Form1.cs
+++ IBoxUpdater/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,32 @@ namespace IBoxUpdater
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adress = "http://ibox.su/obnovlenie-po/radar-detectors/ibox-x6/index.html";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(adress);
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiver = response.GetResponseStream();
+                StreamReader reader = null;
+                if (response.CharacterSet == null)
+                {
+                    reader = new StreamReader(receiver);
+                }
+                else
+                {
+                    reader = new StreamReader(receiver, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = reader.ReadToEnd();
+
+                response.Close();
+                reader.Close();
+            }
         }
     }
 }
